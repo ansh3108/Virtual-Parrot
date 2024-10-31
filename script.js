558fa36cffb88d8ce907e16abe0408081ef4f4d7
Ansh Kumar <anshkumar9686@gmail.com>
2024-10-31 12:19:50 +0530

    some fixes

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-function squawk() {
-    fetch('https://api.quotable.io/random')
-    .then(response => response.json)
-    .then(data => {
-        document.getElementById('quote').textContent = data.textContent;
+let quotes = [];
 
+fetch('https://type.fit/api/quotes')
+    .then(response => response.json())
+    .then(data => {
+        quotes=data;
+    })
+    .catch(error =>{
+        console.error('Cant fetch quotes:', error);
+        document.getElementById('quote').textContent = "Couldn't load quotes.";
+});
+
+function squawk(){
+    if(quotes.length>0){
+        const random= quotes[Math.floor(Math.random()* quotes.length)].text; //get quote's text from quotes array
+        document.getElementById('quote').textContent=randomQuote;
         const audio=new Audio('squawk.mp3');
-    })
-    .catch(error => {
-        console.error('Error fetching quote:', error);
-        document.getElementById('quote').textContent = "Couldn't fetch a quote at this moment! try again later"
-    })
+        audio.play();
+    } else{
+        document.getElementById('quote').textContent="Crunching more quotes!!"
+    }
+    
 }
